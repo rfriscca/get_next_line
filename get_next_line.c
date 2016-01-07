58e2c8febf551ab889ab0a9ab0980620eb489693
Remi FRISCCA <rfriscca@e3r10p3.42.fr>
2016-01-07 15:36:26 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,26 @@
 /*   By: rfriscca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/30 13:45:26 by rfriscca          #+#    #+#             */
-/*   Updated: 2016/01/07 14:17:02 by rfriscca         ###   ########.fr       */
+/*   Updated: 2016/01/07 15:36:22 by rfriscca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int		read_line(int i, int size, char **line, char *buf)
+{
+	char	*line1;
+
+	line1 = (char*)malloc(sizeof(*line1) * size);
+	while (buf[i] != '\n' && buf[i])
+	{
+		line1[i] = buf[i];
+		++i;
+	}
+	if ((*line = ft_strjoin(*line, line1)) == NULL)
+		return (-1);
+	return (i);
+}
 
 int		get_next_line(int const fd, char **line)
 {
@@ -23,8 +38,8 @@ int		get_next_line(int const fd, char **line)
 			return (-1);
 	while (buf[i] != '\n' && size != 0)
 	{
-		line[0][i] = buf[i];
-		++i;
+		if ((i = read_line(i, size, line, buf)) == -1)
+			return (-1);
 		if (i == size)
 		{
 			if ((size = read(fd, buf, BUF_SIZE)) == -1)
@@ -32,6 +47,7 @@ int		get_next_line(int const fd, char **line)
 			i = 0;
 		}
 	}
+	++i;
 	if (size == 0)
 		return (0);
 	return (1);
